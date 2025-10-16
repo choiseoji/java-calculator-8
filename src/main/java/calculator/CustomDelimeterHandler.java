d6b6a4ab885d07954e2d7d2fcfa32338ec7f3ce0
choiseoji <choiseoji123@naver.com>
2025-10-16 16:22:27 +0900

    feat: 커스텀 구분자에 숫자 들어오면 에러 반환

--- src/main/java/calculator/CustomDelimeterHandler.java
+++ src/main/java/calculator/CustomDelimeterHandler.java
@@ -14,7 +14,10 @@ public class CustomDelimeterHandler {
         int endOfDelimeter = command.indexOf("\\n");
         if (endOfDelimeter == -1)
             throw new IllegalArgumentException("커스텀 구분자는 '\\n'으로 끝나야 합니다.");
-        return command.substring(2, endOfDelimeter);
+
+        String delimeter = command.substring(2, endOfDelimeter);
+        validateDelimeter(delimeter);
+        return delimeter;
     }
 
     public String removeDelimiterDeclaration(String command) {
@@ -23,5 +26,12 @@ public class CustomDelimeterHandler {
         if (endOfDelimeter == -1)
             throw new IllegalArgumentException("커스텀 구분자는 '\\n'으로 끝나야 합니다.");
         return command.substring(endOfDelimeter + 2);
+    }
+
+    private void validateDelimeter(String command) {
+
+        if (command.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException("커스텀 구분자에 숫자를 넣을 수 없습니다.");
+        }
     }
 }
