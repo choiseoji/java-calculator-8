b7b3af9d25b1886d30d419195ea57e03e8510ceb
choiseoji <choiseoji123@naver.com>
2025-10-18 15:14:10 +0900

    refact: CustomDelimeterHandler 에서 구분자 여부 직접 확인하도록 변경

--- src/main/java/calculator/CustomDelimeterHandler.java
+++ src/main/java/calculator/CustomDelimeterHandler.java
@@ -2,14 +2,10 @@ package calculator;
 
 public class CustomDelimeterHandler {
 
-    public boolean hasCustomDelimeter(String command) {
-
-        if (command.startsWith("//"))
-            return true;
-        return false;
-    }
-
     public String extractCustomDelimeter(String command) {
+
+        if (!hasCustomDelimeter(command))
+            return null;
 
         int endOfDelimeter = command.indexOf("\\n");
         if (endOfDelimeter == -1)
@@ -17,20 +13,32 @@ public class CustomDelimeterHandler {
 
         String delimeter = command.substring(2, endOfDelimeter);
         validateDelimeter(delimeter);
+
         return delimeter;
     }
 
     public String removeDelimiterDeclaration(String command) {
 
+        if (!hasCustomDelimeter(command))
+            return command;
+
         int endOfDelimeter = command.indexOf("\\n");
         if (endOfDelimeter == -1)
             throw new IllegalArgumentException("커스텀 구분자는 '\\n'으로 끝나야 합니다.");
+
         return command.substring(endOfDelimeter + 2);
     }
 
-    private void validateDelimeter(String command) {
+    private boolean hasCustomDelimeter(String command) {
 
-        if (command.chars().allMatch(Character::isDigit)) {
+        if (command.startsWith("//"))
+            return true;
+        return false;
+    }
+
+    private void validateDelimeter(String delimeter) {
+
+        if (delimeter.chars().allMatch(Character::isDigit)) {
             throw new IllegalArgumentException("커스텀 구분자에 숫자를 넣을 수 없습니다.");
         }
     }
