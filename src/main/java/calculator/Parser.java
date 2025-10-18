0b23cfd54cfe7dbea4a4ef04be864e4b2f2c269e
choiseoji <choiseoji123@naver.com>
2025-10-18 15:08:41 +0900

    refact: 구분자를 처리하는 흐름은 CustomDelimeterHandler에서 하도록 Parser의 run 흐름 변경

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -10,18 +10,14 @@ public class Parser {
 
     public String[] run(String command) {
 
-        String delimeter = null;
-        CustomDelimeterHandler customDelimeterHandler = new CustomDelimeterHandler();
-        if (customDelimeterHandler.hasCustomDelimeter(command)) {
-            delimeter = customDelimeterHandler.extractCustomDelimeter(command);
-            command = customDelimeterHandler.removeDelimiterDeclaration(command);
-        }
+        String customDelimeter = customDelimeterHandler.extractCustomDelimeter(command);
+        String content = customDelimeterHandler.removeDelimiterDeclaration(command);
 
-        String regex = getRegularExpression(delimeter);
-        return command.split(regex);
+        String regex = buildRegex(customDelimeter);
+        return content.split(regex);
     }
 
-    private String getRegularExpression(String delimeter) {
+    private String buildRegex(String delimeter) {
 
         String defaultDelimeter = ",|:";
         if (delimeter != null)
