8d9c9dd921e9719efcef255f49091b3f7fb60b06
choiseoji <choiseoji123@naver.com>
2025-10-19 23:18:58 +0900

    test: 문자열 덧셈기 예외 및 커스텀 구분자 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -24,6 +24,78 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 빈_문자열을_입력하면_0_반환() {
+        assertSimpleTest(() -> {
+            run("");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 구분자가_없으면_숫자를_그대로_반환() {
+        assertSimpleTest(() -> {
+            run("123");
+            assertThat(output()).contains("결과 : 123");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자를_사용해_여러_숫자를_계산() {
+        assertSimpleTest(() -> {
+            run("//a\\n1a2,3:4a5");
+            assertThat(output()).contains("결과 : 15");
+        });
+    }
+
+    @Test
+    void 구분자가_연속해서_등장하면_예외발생() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,:2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 구분자로_시작하면_예외발생() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 구분자로_끝나면_예외발생() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2,3,"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 구분자_사이에_숫자가_아닌_문자가_있으면_예외발생() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("2,a,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자_선언이_두_번_등장하면_예외발생() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\\n//;\\n1;2;3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 커스텀_구분자_선언이_완전하지_않으면_예외발생() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//;\n1;2;3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
